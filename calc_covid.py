133dab7b8074ff4a2f136c354389300e1b235fd9
Linnea <linnea.faxen@gmail.com>
2020-10-16 22:54:50 +0200

    Updated calc_covid to also create 7-day trend plots and put all plots into one figure. Started working on a new file, return_to_play_tracker to try to track progress according to WFTDA Return to Play guideline, still work in progress.

--- calc_covid.py
+++ calc_covid.py
@@ -1,31 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import wget
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import datetime
 # Other dependencies: xlrd needed for reading Excel-file into Python
 
 # REQUIRED INPUT (EDIT HERE) #
 region = 'Östergötland'
+region_trend = 'Östergötland'
 region_tot = 461583
 
-#kommun = 'Norrköping'
-#kommun_tot = 143302
 kommun = 'Linköping'
 kommun_tot = 163051
+
 filepath = '/path/to/where/you/want/to/store/the/data'
 
-# Flera regioner och kommuner i en lista, kör kommun_plot på varje
-
 #------ CODE BEGINS HERE ------ #
-
+width = 0.2
+height = 0.3
 kommun_week = round((kommun_tot/100000)*50) # WFTDA calc, max allowable cases per week
 kommun_day = round(kommun_week/14) # WFTDA calc, max allowable cases per day
 region_week = round((region_tot/100000)*50) # WFTDA calc, max allowable cases per week
 region_day = round(region_week/14) # WFTDA calc, max allowable cases per day
 
 url = 'https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data'
-wget.download(url, filepath + 'Covid19.xlsx') # Download data from Folkhälsomyndigheten
+#wget.download(url, filepath + 'Covid19.xlsx') # Download data from Folkhälsomyndigheten
 veckodata_region = pd.read_excel(filepath + 'Covid19.xlsx', sheet_name=6)
 veckodata_kommun = pd.read_excel(filepath + 'Covid19.xlsx', sheet_name=7)
+seven_day_average = pd.read_excel(filepath + 'Covid19.xlsx', sheet_name=0)
 
 def clean_data(data, columns_to_filter):
     data = data.copy(deep=True)
@@ -56,6 +59,39 @@ def calc_twoweek_region(data):
                       data.iloc[i]['Antal_fall_vecka'])
     return average
 
+def trend_plot(date):
+    # Filter out data, trend_filt is a dataframe with column 'Statistikdatum' and 'Region'
+    # Statistikdatum entries are datetime64 objects
+    trend_filt = seven_day_average[['Statistikdatum', region_trend]]
+    tmp = trend_filt[trend_filt['Statistikdatum'] <= date]
+    # Last 7 days
+    end_date = date - datetime.timedelta(days=7)
+    one_week = tmp[tmp['Statistikdatum'] > end_date]
+
+    # Calculate trend over last 7 days
+    y = np.array(one_week[region].tolist())
+    x = [1, 2, 3, 4, 5, 6, 7]
+    x_reshaped = np.array(x).reshape((-1,1))
+    model = LinearRegression().fit(x_reshaped, y)
+    y_trend = model.intercept_ + model.coef_*x
+
+    ax = plt.gca()
+    plt.plot(x, y_trend, color = 'red')
+    plt.bar(x, y)
+    plt.ylim(0,100)
+    
+    plt.title('7-dagars trend från: ' + str(end_date.strftime('%Y-%m-%d'))
+              + ' till: ' + str(date.strftime('%Y-%m-%d')) + ' i ' + region)
+    plt.xlabel('Datum')
+    plt.ylabel('Antal nya fall')
+    plt.legend(['7-dagars trend', 'Antal fall per dag'], loc = 'upper left')
+
+    # Add dates to the x-ticks
+    plt.xticks(x, one_week['Statistikdatum'].map(lambda x: x.strftime('%Y-%m-%d')), rotation=20)
+    # print out the intercept and coef
+    print('Coefficient (a): ' + str(model.coef_))
+    print('Offset (b): ' + str(model.intercept_))
+
 def kommun_plot():
     # Get the right rows (rows where kolumn KnNamn == Linköping)
     kommun_filt = veckodata_kommun[veckodata_kommun["KnNamn"] == kommun]
@@ -68,29 +104,26 @@ def kommun_plot():
     kommun_filt.insert(loc = 0, column = "average", value = calc_twoweek(kommun_filt))
     
     # Create the plot, x: week numbers [veckonummer], y: number of new cases [nya_fall_vecka]
-    plt.figure(1)
+    plt.subplot(221)
     ax = plt.gca()
-    kommun_filt.plot(x = 'veckonummer', y = 'target', color = 'purple', use_index = True, ax = ax)
-    kommun_filt.plot(x = 'veckonummer', y = 'average', use_index = True, ax = ax, color = 'green')   
-    kommun_filt.plot(x = 'veckonummer', y = 'target2', color = 'purple', linestyle = 'dashed',
+    kommun_filt.plot(x = 'veckonummer', y = 'target', color = 'blue', use_index = True, ax = ax)
+    kommun_filt.plot(x = 'veckonummer', y = 'average', use_index = True, ax = ax, color = 'blue',
+                     linestyle = 'dashed')   
+    kommun_filt.plot(x = 'veckonummer', y = 'target2', color = 'orange',
                      use_index = True, ax = ax)
     kommun_filt.plot(x = 'veckonummer', y = 'nya_fall_vecka', linestyle = 'dashed',
-                     color = 'green', use_index = True, ax=ax)
+                     color = 'orange', use_index = True, ax=ax)
    
     plt.gca().set_xticks(kommun_filt["veckonummer"].unique())
     plt.title(kommun)
     plt.xlabel('Veckonummer')
     plt.ylabel('Antal')
-    L=plt.legend()
+    L=plt.legend(loc = 'upper left')
     L.get_texts()[0].set_text('Max antal per 14-dagars-period (WFTDA)')
     L.get_texts()[1].set_text('Nya fall per 14-dagars-period')
     L.get_texts()[2].set_text('WFTDA-värdet delat med 2')
     L.get_texts()[3].set_text('Nya fall per vecka')
  
-    plt.savefig(filepath + 'plot_' + kommun + '.png')
-
-kommun_plot()
-
 def region_plot():
     # Get the right rows (rows where kolumn region == Östergötland)
     region_filt = veckodata_region[veckodata_region["Region"] == region]
@@ -103,14 +136,15 @@ def region_plot():
     region_filt.insert(loc = 0, column = "average", value = calc_twoweek_region(region_filt))
    
     # Create the plot, x: week numbers [veckonummer], y: number of new cases [nya_fall_vecka]
-    plt.figure(2)
+    plt.subplot(222)
     ax = plt.gca()
-    region_filt.plot(x = 'veckonummer', y = 'target', color = 'purple', use_index = True, ax = ax)
-    region_filt.plot(x = 'veckonummer', y = 'average', use_index = True, ax = ax, color = 'green')   
-    region_filt.plot(x = 'veckonummer', y = 'target2', color = 'purple', linestyle = 'dashed',
+    region_filt.plot(x = 'veckonummer', y = 'target', color = 'blue', use_index = True, ax = ax)
+    region_filt.plot(x = 'veckonummer', y = 'average', use_index = True, ax = ax, color = 'blue',
+                     linestyle = 'dashed',)   
+    region_filt.plot(x = 'veckonummer', y = 'target2', color = 'orange',
                      use_index = True, ax = ax)
     region_filt.plot(x = 'veckonummer', y = 'Antal_fall_vecka', linestyle = 'dashed',
-                     color = 'green', use_index = True, ax=ax)
+                     color = 'orange', use_index = True, ax=ax)
     #region_filt.plot(x = 'veckonummer', y = 'Antal_intensivvårdade_vecka', use_index = True, ax=ax)
     #region_filt.plot(x = 'veckonummer', y = 'Antal_avlidna_vecka', use_index = True, ax=ax)
 
@@ -118,14 +152,46 @@ def region_plot():
     plt.title(region)
     plt.xlabel('Veckonummer')
     plt.ylabel('Antal')
-    L=plt.legend()
+    L=plt.legend(loc = 'upper left')
     L.get_texts()[0].set_text('Max antal per 14-dagars-period (WFTDA)')
     L.get_texts()[1].set_text('Nya fall per 14-dagars-period')
     L.get_texts()[2].set_text('WFTDA-värdet delat med 2')
     L.get_texts()[3].set_text('Nya fall per vecka')
     #L.get_texts()[4].set_text('Nya intensivvårdade per vecka')
     #L.get_texts()[5].set_text('Avlidna per vecka')
-    
-    plt.savefig(filepath + 'plot_' + region + '.png')
 
+#------ MAIN ------ #
+# This code produces the following plots:
+#  - 7-day trend for the last 7-days
+#  - 7-day trend for the previous 7-day period
+#  - Number of cases per week for a commune, and the WFTDA benchmark
+#  - Number of cases per week for a region, and the WFTDA benchmark
+
+# Setup the figure that will hold all four plots
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20, 10))
+plt.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=width, hspace=height)
+plt.clf()
+
+#------ TREND ------ #
+# For the trend, create the start-date for each 7-day period to plot
+end_date_now = datetime.datetime.today() - datetime.timedelta(days=1)
+end_date_last_week = end_date_now - datetime.timedelta(days=7)
+
+# Plot the 7-day trend for the previous 7-day period
+plt.subplot(223)   
+trend_plot(end_date_last_week)
+
+# Plot the 7-day trend based on the last 7 days
+plt.subplot(224)
+trend_plot(end_date_now)
+
+#------ COMMUNE ------ #
+# Plot the number of cases per week for a commune
+kommun_plot()
+
+#------ REGION ------ #
+# Plot the number of cases per week for a region
 region_plot()
+
+# Save the figure
+plt.savefig(filepath + 'overall_plot.png')
---
+++ return_to_play_tracker.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import wget
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import datetime
+# Other dependencies: xlrd needed for reading Excel-file into Python
+
+# REQUIRED INPUT (EDIT HERE) #
+region = 'Östergötland'
+region_tot = 461583
+filepath = '/path/to/where/you/want/to/store/the/data'
+
+start_date = datetime.datetime.strptime('2020-08-17', '%Y-%m-%d')
+
+#------ CODE BEGINS HERE ------ #
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   
+region_two_week = round((region_tot/100000)*50) # WFTDA calc, max allowable cases per week
+region_day = round(region_two_week/14) # WFTDA calc, max allowable cases per day
+
+url = 'https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data'
+#wget.download(url, filepath + 'Covid19.xlsx') # Download data from Folkhälsomyndigheten
+seven_day_average = pd.read_excel(filepath + 'Covid19.xlsx', sheet_name=0)
+
+def calc_twoweek_region(data):
+    data = data.copy(deep=True)
+    num_rows = len(data.index)
+    average = [0] * num_rows
+    for i in range(1, num_rows):
+        average[i] = (data.iloc[i-1]['Antal_fall_vecka'] +
+                      data.iloc[i]['Antal_fall_vecka'])
+    return average
+
+# Start
+tier = 0
+days = 0
+
+end = datetime.datetime.today()
+date_list = [start_date + datetime.timedelta(days=x) for x in range(0, (end-start_date).days)]
+
+for date in date_list:    
+    # Filter out data, trend_filt is a dataframe with column 'Statistikdatum' and 'Region'
+    # Statistikdatum entries are datetime64 objects
+    trend_filt = seven_day_average[['Statistikdatum', region]]
+    tmp = trend_filt[trend_filt['Statistikdatum'] <= date]
+    # Last 14 days
+    two_weeks = tmp[tmp['Statistikdatum'] > (date - datetime.timedelta(days=14))]
+    # Last 7 days
+    one_week = tmp[tmp['Statistikdatum'] > (date - datetime.timedelta(days=7))]
+
+    # Cases over 14 days
+    cases = two_weeks[region].sum()
+
+    # Calculate trend over last 7 days
+    y = np.array(one_week[region].tolist())
+    x = [1, 2, 3, 4, 5, 6, 7]
+    x_reshaped = np.array(x).reshape((-1,1))
+    model = LinearRegression().fit(x_reshaped, y)
+    y_trend = model.intercept_ + model.coef_*x
+
+    # Tier tracking
+    if tier == 0:
+        if cases < region_two_week:
+            tier = 1
+            action = 'Step up to Tier 1 from Baseline Conditions'
+            days = 1
+        else:
+            action = 'Remain, Baseline Condition of number of allowed cases not fulfilled'
+    elif tier == 1:
+        # Add a count, number of days since 7-day trend, when it hits 7, affects action!
+        # days_since_increase, always increases if it is already more than 0
+        # set to 1 at first positive trend
+        # reset to 0 if OK after 7 days
+        if model.coef_ > 0:
+            action = 'Stay at Tier 1, 7-day increase trend'
+            days = 1
+        else:
+            if days >= 14:
+                action = 'Step up to Tier 2 from Tier 1, 14-days passed with no 7-day increase trend'
+                tier = 2
+                days = 1
+            else:
+                action = 'Stay at Tier 1, no 7-day increase, but 14 days has not passed yet'
+                days = days + 1
+
+    # Printing
+    print('#----------' + str(date.strftime('%Y-%m-%d')) + '---------#')
+    print(f"{color.BOLD}Action: {color.END}{action}")
+    print(f"{color.BOLD}Trend: {color.END}{model.coef_}")
+    print(f"{color.BOLD}Tier: {color.END}{tier} , number of days spent at this tier: {days}")
+    print(f"{color.BOLD}Number of cases over 14-days: {color.END}{cases}")
+    print(f"{color.BOLD}Max number of allowable cases over 14-days (WFTDA):"
+          + f"{color.END}{region_two_week}")
+
+
+# Plot 7-day trend for the last 7-days
+plt.figure(3)
+ax = plt.gca()
+plt.plot(x, y_trend, color = 'red')
+plt.bar(x, y)
+
+plt.title('7 dagars trend')
+plt.xlabel('Dagar')
+plt.ylabel('Antal nya fall')
+plt.axis([0.5, 7.5, 0, 30])
+plt.savefig(filepath + 'plot_trend_' + region + '.png')
